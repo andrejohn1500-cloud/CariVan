88a3a1c79f414c9cd23312d6b20c4f5920395e95
Andre John <andrejohn1500@gmail.com>
2026-05-28 11:01:24 -0400

    Update TestCar.js

--- src/world/TestCar.js
+++ src/world/TestCar.js
@@ -9,20 +9,15 @@ export class TestCar {
   }
 
   _load() {
-    const t = this.roadSystem.getCarTransform(
-      this.roadSystem.totalLength * 0.001, -40
-    );
-
     SceneLoader.ImportMesh(
       '', './assets/', '2009_honda_civic_type_r_fd2_custom.glb', this.scene,
       (meshes) => {
         if (!meshes.length) return;
         const root = meshes[0];
         root.scaling    = new Vector3(10, 10, 10);
-        root.position   = t.position.clone();
-        root.position.y = 25.5;
-        root.rotation.y = t.heading + (Math.PI / 2);
-        console.log('[CariVan] FD2 loaded');
+        root.position   = new Vector3(4650, 25.5, -9350);
+        root.rotation.y = 0;
+        console.log('[CariVan] FD2 loaded at hardcoded position');
       },
       null,
       (s, msg) => console.warn('[CariVan] FD2 failed:', msg)
